37ac0b2808ca54441a25c98273b7563c804e754e
jojiGh <ighandilyan@gmail.com>
2022-02-16 02:17:17 -0500

    Q1

---
+++ practice1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int b;
+	printf("Enter the first number:");
+	scanf("%d", &a);
+	printf("Enter the second number:");
+	scanf("%d", &b);
+	printf("%d", a+b);
+	return 0;
+}
