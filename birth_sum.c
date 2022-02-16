9aea8d272ab1c0aab44fd830aadcf7a7ce7f3fdb
jojiGh <ighandilyan@gmail.com>
2022-02-16 02:28:53 -0500

    Q1

---
+++ birth_sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int b_y = 2002;
+	int b_m = 6;
+	int b_d = 20;
+	int sum = b_y + b_m + b_d;
+	printf("The sum of your birthday date is %d", sum);
+	return 0;
+}
